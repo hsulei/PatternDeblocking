6d496e93454488958ca17acde0d78d7902f4ca6d
hsulei <466977183@qq.com>
2016-10-17 22:11:09 +0800

    创建项目

---
+++ PatternDeblocking/app/src/main/java/com/hsulei/portraitchoose/patterndeblocking/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hsulei.portraitchoose.patterndeblocking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
